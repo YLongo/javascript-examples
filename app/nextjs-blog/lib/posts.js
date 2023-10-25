3e64653e8a64061c77f5847c73fa512d94fe18ec
YLongo <yhlona523@gmail.com>
2023-10-25 16:38:47 +0800

    next.js

--- app/nextjs-blog/lib/posts.js
+++ app/nextjs-blog/lib/posts.js
@@ -1,10 +1,13 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import {remark} from "remark";
+import html from 'remark-html';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
 export function getSortedPostsData() {
+    console.log(postsDirectory);
     const fileNames = fs.readdirSync(postsDirectory);
 
     const allPostsData = fileNames.map(fileName => {
@@ -26,4 +29,46 @@ export function getSortedPostsData() {
         }
         return -1;
     })
+}
+
+export function getAllPostIds() {
+    console.log('getallpostids', postsDirectory);
+    const fileNames = fs.readdirSync(postsDirectory);
+
+    // Returns an array that looks like this:
+    // [
+    //   {
+    //     params: {
+    //       id: 'ssg-ssr'
+    //     }
+    //   },
+    //   {
+    //     params: {
+    //       id: 'pre-rendering'
+    //     }
+    //   }
+    // ]
+
+    return fileNames.map(fileName => {
+        return {
+            params: {
+                id: fileName.replace(/\.md$/, ''),
+            }
+        }
+    })
+}
+
+export async function getPostData(id) {
+    console.log(id);
+    const fullPath = path.join(postsDirectory, `${id}.md`);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    const matterResult = matter(fileContents);
+
+    const processedContent = await remark().use(html).process(matterResult.content);
+    const contentHtml = processedContent.toString();
+
+    return {
+      id, contentHtml, ...matterResult.data,
+    };
 }
